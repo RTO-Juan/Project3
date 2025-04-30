4366d639cd3585b8f8891091bc5c1000992be710
RTO-Juan <jperezchica@hawk.iit.edu>
2025-04-30 00:59:02 -0500

    adding markers and panto feature

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
-let map;
+var map;
 
-const locations = {
+var locations = {
   park: { lat: 41.8826, lng: -87.6226 },   
   pier: { lat: 41.8916, lng: -87.6079 },   
   tower: { lat: 41.8789, lng: -87.6359 }   
 };
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 41.8781, lng: -87.6298 }, 
+    zoom: 13
+  });
+
+  var marker1 = new google.maps.Marker({
+    position: locations.park,
+    map: map,
+    title: "Millennium Park"
+  });
+
+  var marker2 = new google.maps.Marker({
+    position: locations.pier,
+    map: map,
+    title: "Navy Pier"
+  });
+
+  var marker3 = new google.maps.Marker({
+    position: locations.tower,
+    map: map,
+    title: "Willis Tower"
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+function panTo(key) {
+  if (locations[key]) {
+    map.panTo(locations[key]);
+  }
+}
 
 
